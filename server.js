9dada37931ff9764de837047e0a1b889c45252b0
LuisFPicazo <luis.picazo@ids.com.mx>
2025-09-18 20:28:07 -0600

    Primer commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./src/routes');
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
